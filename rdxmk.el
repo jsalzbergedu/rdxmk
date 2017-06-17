62fcd019ab7d5f77a550aa1f8bfedc30a51ad84d
Jacob Salzberg <jsalzbergedu@yahoo.com>
2017-06-17 08:05:36 -0400

    Reworked rdxmk so that it can work as a package on MELPA

--- rdxmk.el
+++ rdxmk.el
@@ -1,21 +1,23 @@
-;;; rdxmk.el --- A small set of tools for redox development in Gnu Emacs
+;;; rdxmk.el --- A small set of tools for redox developments
 
 ;; Copyright (C) 2017 Jacob Salzberg
 
 ;; Author: Jacob Salzberg <jsalzbergedu@yahoo.com>
 ;; Version: 0.10
-;; Keywords: redox
+;; Keywords: redox, convenience, tools
 ;; URL: https://github.com/jsalzbergedu/rdxmk
 
 ;;; Commentary:
 
 ;; rdxmk provides a few tools to make redox development easier in Emacs.
-;; You can run make with no arguments (M-x make-narg RET), make with arguments
-;; (M-x make-warg RET arg RET), run the built in make-qemu and make-all, or use cookbook
-;; (M-x Cookbook RET package RET option RET.) Finally, you can stop
+;; rdxmk provides a few convenience functions for building redox.
+;; You can run make with no arguments (M-x rdxmk-make-narg RET), make with arguments
+;; (M-x rdxmk-make-warg RET arg RET), run the built in rdxmk-make-qemu and rdxmk-make-all, and use cookbook
+;; (M-x Rdxmk-Cookbook RET package RET option RET.) Finally, you can stop
 ;; Emacs from inserting files all over your redox/ directory by
 ;; going to rdxmk's customization group with `M-x customize-group RET rdxmk RET`
-;; and setting `lockfile-no-pollute` to t.
+;; and setting `lockfile-no-pollute` to t. For more information, go to the
+;; documentation with `C-h i`
 
 ;;; Installation
 
@@ -29,9 +31,7 @@
 
 ;;; Code:
 
-;;(require 'cl) ;; Make sure to uncomment this line if you don't have it already
-
-(defun up-a-dir (dir)
+(defun rdxmk-up-a-dir (dir)
   "Helper function to return the directory one up from DIR."
   ;; As far as I can tell, this is the idiomatic way to
   ;; "traverse a filesystem tree," even though the built in
@@ -39,124 +39,137 @@
   (file-name-directory (directory-file-name dir)))
 
 
-(defun recurse-for-file (file dir)
-  "Calls itself until it runs into FILE or until DIR has a length of 1.
+(defun rdxmk-recurse-for-file (file dir)
+  "Will call itself FILE or until DIR is found or has a length of 1.
 If DIR has a length of 1 and FILE is not found, returns nil.
 If FILE is found, returns the directory and file."
   (if (= (length dir) 1)
       (if (file-exists-p (expand-file-name file dir))
 	  (expand-file-name file dir)
-	())
+	'())
     (if (file-exists-p (expand-file-name file dir))
 	(expand-file-name file dir)
-      (recurse-for-file file (up-a-dir dir)))))
+      (rdxmk-recurse-for-file file (rdxmk-up-a-dir dir)))))
 
-(defun get-closest-pathname (&optional file)
- "By default, searches for the nearest makefile, recursivley going up to parent directories.
+(defun rdxmk-get-closest-pathname (&optional file)
+ "By default, RDXMK-GET-CLOSEST-PATHNAME will find the closest makefile.
+Will recursivley go up to parent directories.
 If FILE is specified, searches for FILE in the same way."
  (if file
-     (recurse-for-file file default-directory) ; recurse until it finds FILE goes here
-     (recurse-for-file "Makefile" default-directory))) ; recurse until it finds Makefile goes here
+     (rdxmk-recurse-for-file file default-directory) ; recurse until it finds FILE goes here
+     (rdxmk-recurse-for-file "Makefile" default-directory))) ; recurse until it finds Makefile goes here
 
-(defun make-qemu ()
+(defun rdxmk-make-qemu ()
   "Go up to the root makefile of the redox project, run make qemu."
   (interactive)
   (shell-command
    (concat
-    "make -C " (substring (get-closest-pathname "Makefile") 0 -8) " qemu &")
+    "make -C " (substring (rdxmk-get-closest-pathname "Makefile") 0 -8) " qemu &")
    (get-buffer-create "*Redox Build Output*")
-   (get-buffer "*Redox Build Output*"))) 
+   (get-buffer "*Redox Build Output*")))
 
-(defun make-all ()
+(defun rdxmk-make-all ()
   "Go up to the root makefile of the redox project, run make all."
   (interactive)
   (shell-command
    (concat
-    "make -C " (substring (get-closest-pathname "Makefile") 0 -8) " all &")
+    "make -C " (substring (rdxmk-get-closest-pathname "Makefile") 0 -8) " all &")
    (get-buffer-create "*Redox Build Output*")
-   (get-buffer "*Redox Build Output*"))) 
+   (get-buffer "*Redox Build Output*")))
 
 
-(defun make-narg ()
+(defun rdxmk-make-narg ()
   "Go up to the root makefile of the redox project, run make without arguments."
   (interactive)
   (shell-command
    (concat
-    "make -C " (substring (get-closest-pathname "Makefile") 0 -8) " &")
+    "make -C " (substring (rdxmk-get-closest-pathname "Makefile") 0 -8) " &")
    (get-buffer-create "*Redox Build Output*")
-   (get-buffer "*Redox Build Output*"))) 
+   (get-buffer "*Redox Build Output*")))
 
 
-(defun make-warg (arg)
+(defun rdxmk-make-warg (arg)
   "Go up to the root makefile of the redox project, run make with the given argument, ARG."
-  (interactive
-   (list (read-string "Type argument to pass to make: ")) arg)
+  (interactive "sType argument to pass to make: ")
   (shell-command
    (concat
-    "make -C " (substring (get-closest-pathname "Makefile") 0 -8) " " arg " &")
+    "make -C " (substring (rdxmk-get-closest-pathname "Makefile") 0 -8) " " arg " &")
    (get-buffer-create "*Redox Build Output*")
-   (get-buffer "*Redox Build Output*"))) 
+   (get-buffer "*Redox Build Output*")))
 
-(defun cookbook (package op)
-  "Run cookbook.sh with the specified package and option.
-  Options include the following:
-  dist
-  distclean
-  build
-  clean
-  fetch
-  unfetch
-  publish
-  unpublish
-  stage
-  unstage
-  tar
-  untar
-  update
-  version"
+(defun rdxmk-cookbook (package op)
+  "Run rdxmk-cookbook.sh with the specified PACKAGE and OP(tion).
+Options include the following:
+dist
+distclean
+build
+clean
+fetch
+unfetch
+publish
+unpublish
+stage
+unstage
+tar
+untar
+update
+version"
   (interactive "sPackage: \nsOption: ")
   (shell-command
    (concat
-    (substring (get-closest-pathname "Makefile") 0 -8) "/cookbook/cook.sh " package " " op " &")
-   (get-buffer-create "*Cookbook Output*")
-   (get-buffer "*Cookbook Output*"))) 
+    (substring (rdxmk-get-closest-pathname "Makefile") 0 -8) "/rdxmk-cookbook/cook.sh " package " " op " &")
+   (get-buffer-create "*Rdxmk-Cookbook Output*")
+   (get-buffer "*Rdxmk-Cookbook Output*")))
 
-(define-minor-mode redox-mode
+(define-minor-mode rdxmk-redox-mode
   "Redox mode - adds a hook for working with redox projects."
   nil ;; redox-mode is must be set true to be on
   " redox-mode ";; shows redox-mode on the mode line
   nil) ;; redox-mode does not have a keymap
   
-(defun redox-togg-cond ()
-  "Toggles redox-mode on if a file being visited is under redox/."
+(defun rdxmk-redox-togg-cond ()
+  "Toggle `rdxmk-redox-mode` on if a file being visited is under redox/."
   (if (string-match
        (regexp-quote "redox/")
        default-directory)
-     (redox-mode 1) 
-    (redox-mode 0)))
+     (rdxmk-redox-mode 1)
+    (rdxmk-redox-mode 0)))
 
-(add-hook 'text-mode-hook 'redox-togg-cond)
-(add-hook 'after-change-major-mode-hook 'redox-togg-cond)
+(add-hook 'text-mode-hook 'rdxmk-redox-togg-cond)
+(add-hook 'after-change-major-mode-hook 'rdxmk-redox-togg-cond)
 
 ;;;###autoload
 (defgroup rdxmk nil
   "rdxmk's customization group"
   :group 'programming)
 
-(defcustom lockfile-no-pollute nil
-  "If non nil, stop Emacs from making lockfiles when redox-mode hook is run and stop Emacs from making auto saves and backups, which all can mess up redox' build system." 
-  :type '(boolean :tag "backup-no-pollute" lockfile-no-pollute)
-  :group 'rdxmk)
+(defcustom rdxmk-lockfile-no-pollute nil
+  "If non nil, stop Emacs from making lockfiles when `rdxmk-redox-mode-hook` is run and stop Emacs from making auto saves and backups, which all can mess up redox' build system."
+  :group 'rdxmk
+  :type 'boolean)
 
-(defun depollute-cond ()
-  "If `lockfile-no-pollute` is t, inhibit backups for the buffer and set `create-lockfiles` and `auto-save-default` to nil when redox-mode is run"
-  (if (lockfile-no-pollute)
-      ((setq create-lockfiles nil)
-       (setq auto-save-default nil)
-       (set (make-local-variable 'backup-inhibited) t))
-    ()))
+(defun rdxmk-reminder-message ()
+  "If `create-lockfiles` is non-nil, remind the user to set it to nil.
+Otherwise, returns nil."
+  (if create-lockfiles
+      (message "Please set the value create-lockfiles to nil so redox will build properly.")
+    '()))
 
-(add-hook 'redox-mode 'depollute-cond)
+(defun rdxmk-depollute-cond ()
+  "If `rdxmk-lockfile-no-pollute` is t, inhibit backups for the buffer and set `create-lockfiles` and `auto-save-default` to nil when `rdxmk-redox-mode` is run."
+  ;; (if (rdxmk-lockfile-no-pollute)
+  ;;     ((rdxmk-reminder-message)
+  ;;      (setq auto-save-default nil)
+  ;;      (set (make-local-variable 'backup-inhibited) t))
+  ;;   '()))
+  (if rdxmk-lockfile-no-pollute
+      (progn
+	(rdxmk-reminder-message)
+	(setq auto-save-default nil)
+	(set (make-local-variable 'backup-inhibited) t))
+    '()))
+
+(add-hook 'rdxmk-redox-mode 'rdxmk-depollute-cond)
 
 (provide 'rdxmk)
-;;; rdxmk ends here
+;;; rdxmk.el ends here
